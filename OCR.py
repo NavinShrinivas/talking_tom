36bb696bfdd08065ea570f3ac633168f6a7a96d6
StraightCat <42774281+NavinShrinivas@users.noreply.github.com>
2021-01-24 09:20:35 +0530

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -3,7 +3,7 @@ import cv2
 
 def process_image():
     filename=input("ENTER FILE NAME TO READ FROM :")
-    image=cv2.imread(filename+'.*',0);
+    image=cv2.imread(filename+,0);
     cv2.imshow('image', image)
     cv2.waitKey()
 
